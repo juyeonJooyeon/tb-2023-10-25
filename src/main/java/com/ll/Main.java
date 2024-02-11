e92dc2c2fd941f58ba0e2b60a3851c0828c7add0
juyeonjooyeon <code.paraclete1@gmail.com>
2024-02-11 19:50:38 +0900

    App 클래스 추가

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,15 @@
 package com.ll;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new App().run();
+    }
+}
+
+class App {
+    void run(){
+        System.out.println("프로그램 실행");
     }
 }
